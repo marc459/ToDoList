aac87be2ac76e2ef9ff9438f92c87bc3031c1542
marcos <santosserranomarcos@gmail.com>
2021-03-29 15:47:39 +0200

    Delete functionality

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const title = document.getElementById('title');
 	const description = document.getElementById('description');
-	const btn = document.getElementById('add');
+	const addBtn = document.getElementById('add');
 	const alert = document.getElementById('alert');
 	const table = document.getElementById('table');
+	const removBtn = document.getElementsByClassName('remove');
+	var taskcounter = 0;
 
-	btn.addEventListener('click', function(){
+	
+	
+
+
+
+
+
+
+
+
+
+	function removeTask(id)
+	{
+		const deleteRow = document.getElementById(id);
+		deleteRow.parentNode.removeChild(deleteRow);
+
+	}
+
+	function addTask()
+	{
 		if (title.value === '' || description.value === '')
 		{
 			alert.classList.remove('d-none');
@@ -13,13 +34,14 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 		else
 		{
-			console.log("Adding to-do");
+			console.log("Adding Actvity");
 			alert.classList.add('d-none');
 			const row = table.insertRow();
+			row.setAttribute('id',`task${taskcounter}`);
 			row.innerHTML = `
 				<td>
 					${title.value}
-            	</td>
+	        	</td>
 	        	<td>
 			  		${description.value}
 	          	</td>
@@ -27,20 +49,24 @@ document.addEventListener('DOMContentLoaded', function() {
 	         		<input type="checkbox">
 	        	</td>
 	        	<td class="text-right">
-	          	<button class="btn btn-primary mb-1">
-	            	<i class="fa fa-pencil"></i>
-	         	</button>
-	          	<button class="btn btn-danger mb-1 ml-1">
-	            	<i class="fa fa-trash"></i>
-	          	</button>
+		          	<button class="btn btn-primary mb-1">
+		            	<i class="fa fa-pencil"></i>
+		         	</button>
 	        	</td>
 			`;
+			let removeBtn = document.createElement('button');
+			removeBtn.classList.add('btn','btn-danger','remove','mb-1','ml-1');
+			removeBtn.setAttribute('id',`btn${taskcounter}`);
+			removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+			row.children[3].appendChild(removeBtn);
+			removeBtn.addEventListener('click', function(e){
+				removeTask(row.getAttribute('id'));
+			});
+			taskcounter++;
 		}
-		
-	})
-	
-	
+	}
 
 
-	
+	addBtn.addEventListener('click', addTask);
 });
+
